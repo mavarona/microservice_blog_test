31339891e9bc721feacb5583286652130ac4e1f4
miguel <mavl@MacBook-Pro-de-Miguel.local>
2020-09-22 01:09:50 +0200

    Add files

--- comments/index.js
+++ comments/index.js
@@ -24,6 +24,7 @@ app.post("/posts/:id/comments", async(req, res) => {
     comments.push({
         id: commentId,
         content,
+        status: 'pending'
     });
     commentsByPostId[req.params.id] = comments;
     await axios.post('http://localhost:4005/events', {
@@ -31,14 +32,40 @@ app.post("/posts/:id/comments", async(req, res) => {
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     });
     res.status(201).send(commentsByPostId[req.params.id]);
 });
 
-app.post('/events', (req, res) => {
-    console.log('Received event', req.body.type);
+app.post('/events', async(req, res) => {
+    const {
+        type,
+        data
+    } = req.body;
+    if (type === 'CommentModerated') {
+        const {
+            id,
+            status,
+            postId,
+            content
+        } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
     res.send({});
 });
 
