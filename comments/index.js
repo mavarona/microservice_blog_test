3edb5389e30445455aeabf4c1f6b6789a38c91bd
miguel <mavl@MacBook-Pro-de-Miguel.local>
2020-09-21 16:52:22 +0200

    Add new microservice

--- comments/index.js
+++ comments/index.js
@@ -4,6 +4,7 @@ const {
     randomBytes
 } = require("crypto");
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,8 +15,7 @@ const commentsByPostId = {};
 app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
-
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async(req, res) => {
     const commentId = randomBytes(4).toString("hex");
     const {
         content
@@ -26,7 +26,20 @@ app.post("/posts/:id/comments", (req, res) => {
         content,
     });
     commentsByPostId[req.params.id] = comments;
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
     res.status(201).send(commentsByPostId[req.params.id]);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received event', req.body.type);
+    res.send({});
 });
 
 app.listen(4001, () => {
