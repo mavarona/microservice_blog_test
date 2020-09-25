4f25b17340dfe1cca240ad2a11ac4f1724ad56ed
miguel <mavl@MacBook-Pro-de-Miguel.local>
2020-09-25 13:11:24 +0200

    change path microservices with name

--- query/index.js
+++ query/index.js
@@ -73,7 +73,7 @@ app.post('/events', (req, res) => {
 
 app.listen(4002, async() => {
     console.log('Listening in port 4002');
-    const res = await axios.get('http://localhost:4005/events');
+    const res = await axios.get('http://event-bus-srv:4005/events');
 
     for (let event of res.data)  {
         handleEvent(event.type, event.data);
